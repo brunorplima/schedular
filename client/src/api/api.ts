d171c57a58e529f18ed96a8e333b5d8f08f063bc
Bruno Lima <brunolima.cad@gmail.com>
2021-02-09 14:43:50 -0700

    postEventApi function created

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import { UnsavedCalendarEvent } from '../interfaces/interfaces';
 
-const URL = 'http://localhost:5000'
+const URL = 'http://localhost:5000/events';
 
- export const getEventsApi = () => axios.get(`${URL}/events`);
+ export const getEventsApi = () => axios.get(URL);
+ export const postEventApi = (newPost: UnsavedCalendarEvent) => axios.post(URL, newPost);
