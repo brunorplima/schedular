5b7ca6d21ed400c7ace4e97dd09a3b518e0ce045
Bruno Lima <brunolima.cad@gmail.com>
2021-02-09 14:37:13 -0700

    allowing event reducer and action to save to db

--- client/src/redux/actions/eventActions.ts
+++ client/src/redux/actions/eventActions.ts
@@ -1,13 +1,23 @@
 import * as actionTypes from '../constants';
 import * as api from '../../api/api';
+import { UnsavedCalendarEvent } from '../../interfaces/interfaces';
 
 export const createGetEventsAction = () => async (disptach: any) => {
    try {
       const { data } = await api.getEventsApi();
-      data.forEach((event:any) => event = new Date(event));
+      data.forEach((event: any) => event = new Date(event));
 
       disptach({ type: actionTypes.GET_EVENTS, payload: data });
    } catch (error) {
       console.error('Error in getEventAcion: ' + error.message);
+   }
+}
+
+export const createPostEventAction = (newPost: UnsavedCalendarEvent) => async (dispatch: any) => {
+   try {
+      const { data } = await api.postEventApi(newPost);
+      dispatch({ type: actionTypes.ADD_EVENTS, payload: data })
+   } catch (error) {
+      console.error('Error in createPostEventAction: ' + error.message);
    }
 }
--- client/src/redux/reducers/eventsReducer.ts
+++ client/src/redux/reducers/eventsReducer.ts
@@ -1,10 +1,12 @@
 import { Action, CalendarEvent } from '../../interfaces/interfaces';
-import { GET_EVENTS } from '../constants';
+import { ADD_EVENTS, GET_EVENTS } from '../constants';
 
 const eventsReducer = (events = [], action: Action): CalendarEvent[] => {
    switch (action.type) {
       case GET_EVENTS:
          return action.payload;
+      case ADD_EVENTS:
+         return [...events, action.payload];
       default:
          return events;
    }
