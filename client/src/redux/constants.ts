aac7c17c586b592c388c1d465cc00e57c925908b
Bruno Lima <brunolima.cad@gmail.com>
2021-02-11 02:26:57 -0700

    more action types constants added

--- client/src/redux/constants.ts
+++ client/src/redux/constants.ts
@@ -1,4 +1,19 @@
 export const GET_EVENTS = 'GET_EVENTS';
-export const ADD_EVENTS = 'ADD_EVENTS';
-export const UPDATE_EVENTS = 'UPDATE_EVENTS';
-export const DELETE_EVENTS = 'DELETE_EVENTS';
+export const ADD_EVENT = 'ADD_EVENT';
+export const UPDATE_EVENTS = 'UPDATE_EVENT';
+export const DELETE_EVENTS = 'DELETE_EVENT';
+
+export const GET_TASKS = 'GET_TASKS';
+export const ADD_TASK = 'ADD_TASK';
+export const UPDATE_TASK = 'UPDATE_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+
+export const GET_REMINDERS = 'GET_REMINDERS';
+export const ADD_REMINDER = 'ADD_REMINDER';
+export const UPDATE_REMINDER = 'UPDATE_REMINDER';
+export const DELETE_REMINDER = 'DELETE_REMINDER';
+
+export const GET_NOTES = 'GET_NOTES';
+export const ADD_NOTE = 'ADD_NOTE';
+export const UPDATE_NOTE = 'UPDATE_NOTE';
+export const DELETE_NOTE = 'DELETE_NOTE';
