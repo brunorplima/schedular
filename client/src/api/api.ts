4661b707bf384c75e93a303de679f8ea12f924af
Bruno Lima <brunolima.cad@gmail.com>
2021-02-11 00:53:52 -0700

    added more request functions to the API

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,7 +1,27 @@
 import axios from 'axios';
-import { UnsavedCalendarEvent } from '../interfaces/interfaces';
+import {
+   UnsavedCalendarEvent,
+   UnsavedTask,
+   UnsavedReminder,
+   UnsavedNote
+ } from '../interfaces/interfaces';
 
-const URL = 'http://localhost:5000/events';
+const EVENTS_URL = 'http://localhost:5000/events';
+const TASKS_URL = 'http://localhost:5000/tasks';
+const REMINDERS_URL = 'http://localhost:5000/reminders';
+const NOTES_URL = 'http://localhost:5000/notes';
 
- export const getEventsApi = () => axios.get(URL);
- export const postEventApi = (newPost: UnsavedCalendarEvent) => axios.post(URL, newPost);
+export const getEventsApi = () => axios.get(EVENTS_URL);
+export const postEventApi = (newPost: UnsavedCalendarEvent) => axios.post(EVENTS_URL, newPost);
+
+
+export const getTasksApi = () => axios.get(TASKS_URL);
+export const postTaskApi = (newTask: UnsavedTask) => axios.post(TASKS_URL, newTask);
+
+
+export const getRemindersApi = () => axios.get(REMINDERS_URL);
+export const postReminderApi = (newReminder: UnsavedReminder) => axios.post(REMINDERS_URL, newReminder);
+
+
+export const getNotesApi = () => axios.get(NOTES_URL);
+export const postNoteApi = (newNote: UnsavedNote) => axios.post(NOTES_URL, newNote);
