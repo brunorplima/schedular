d5488bb0836468c0c6d16737ee354bfae07c0e09
Bruno Lima <brunolima.cad@gmail.com>
2021-02-12 05:22:48 -0700

    unnecessary logs and consts removed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,18 @@
 import React, { useEffect } from 'react';
 import NavBar from './components/navbar/NavBar';
-import { useStore, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { createGetEventsAction } from './redux/actions/eventActions';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CalendarList from './components/calendar-list/CalendarList';
-
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { createGetTasksAction } from './redux/actions/taskActions';
 import { createGetRemindersAction } from './redux/actions/reminderActions';
 
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
 
-   const store = useStore();
    const dispatch = useDispatch();
-   console.log(store.getState().events);
-
-   store.subscribe(() => {
-      console.log(store.getState().events)
-   })
 
    useEffect(() => {
       dispatch(createGetEventsAction());
