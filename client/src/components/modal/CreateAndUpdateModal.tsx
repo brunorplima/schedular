94b652d3f925ee73a2103c692ae42886e40c7ee6
Bruno Lima <brunolima.cad@gmail.com>
2021-02-12 05:15:58 -0700

    split form and functionality into many components

--- client/src/components/modal/CreateAndUpdateModal.tsx
+++ client/src/components/modal/CreateAndUpdateModal.tsx
@@ -1,68 +1,77 @@
-import React, { useState, createRef } from 'react'
-import { Button, Col, Form } from 'react-bootstrap'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { VscClose } from 'react-icons/vsc'
 import { createPostEventAction } from '../../redux/actions/eventActions'
+import { createPostTaskAction } from '../../redux/actions/taskActions'
+import { createPostReminderAction } from '../../redux/actions/reminderActions'
+// import { createPostNoteAction } from '../../redux/actions/notesActions'
 import { useDispatch } from 'react-redux'
-import { BiError } from 'react-icons/bi'
+import { UnsavedCalendarEvent, UnsavedNote, UnsavedReminder, UnsavedTask, DateTime } from '../../interfaces/interfaces'
+import EventForm from './EventForm'
+import TaskForm from './TaskForm'
+import ReminderForm from './ReminderForm'
+import NoteForm from './NoteForm'
 
 import './portal.scss'
-import { UnsavedCalendarEvent } from '../../interfaces/interfaces'
 
-const CreateAndUpdateModal = (props: any) => {
+interface Props {
+   type: string,
+   isOpen: boolean,
+   closePortal: (ev: React.MouseEvent<HTMLDivElement, MouseEvent>, ignoreTarget?: boolean | undefined) => void,
+   closeWhenComplete: () => void
+}
 
-   const [dateTime, setDateTime] = useState({
-      year: -1,
-      month: -1,
-      date: -1,
-      hour: -1,
-      minute: -1
-   });
-   const [name, setName] = useState('');
-   const [location, setLocation] = useState('');
-   const [guests, setGuests] = useState('');
-   const [description, setDescription] = useState('');
-   const [errorList, setErrorList] = useState<string[]>([]);
+const EVENT = 'Event';
+const TASK = 'Task';
+const REMINDER = 'Reminder';
+const NOTE = 'Note';
 
-   const dateControl = createRef<HTMLInputElement>();
-   const timeControl = createRef<HTMLInputElement>();
-   const nameControl = createRef<HTMLInputElement>();
-   const locationControl = createRef<HTMLInputElement>();
-   const guestsControl = createRef<HTMLInputElement>();
-   const descriptionControl = createRef<HTMLTextAreaElement>();
+const CreateAndUpdateModal = ({
+   type,
+   isOpen,
+   closePortal,
+   closeWhenComplete
+}: Props) => {
 
    const dispatch = useDispatch();
 
-
-   function dateInputValue(): string {
+   function dateInputValue(dateTime: DateTime): string {
       const month = dateTime.month >= 10 ? dateTime.month + 1 : `0${dateTime.month + 1}`;
-      const date = dateTime.date >= 10 ? dateTime.date : `0${dateTime.date}`;
+      const date = dateTime.day >= 10 ? dateTime.day : `0${dateTime.day}`;
       return `${dateTime.year}-${month}-${date}`;
    }
 
 
-   function timeInputValue(): string {
+   function timeInputValue(dateTime: DateTime): string {
       const hour = dateTime.hour >= 10 ? dateTime.hour : `0${dateTime.hour}`;
       const minute = dateTime.minute >= 10 ? dateTime.minute : `0${dateTime.minute}`;
       return `${hour}:${minute}`;
    }
 
 
-   function onSetDate(): void {
+   function onSetDate(
+      dateTime: DateTime,
+      setDateTime: React.Dispatch<React.SetStateAction<DateTime>>,
+      dateControl: React.RefObject<HTMLInputElement>
+   ): void {
       const arr = dateControl.current?.value.split('-');
       const dateArr = arr?.map(val => Number(val));
       if (dateArr?.length) {
          const newDate = {
             year: dateArr[0],
             month: dateArr[1] - 1,
-            date: dateArr[2]
+            day: dateArr[2]
          }
          setDateTime({ ...dateTime, ...newDate });
       }
    }
 
 
-   function onSetTime(): void {
+   function onSetTime(
+      dateTime: DateTime,
+      setDateTime: React.Dispatch<React.SetStateAction<DateTime>>,
+      timeControl: React.RefObject<HTMLInputElement>
+   ): void {
       const arr = timeControl.current?.value.split(':');
       const timeArr = arr?.map(val => Number(val));
       if (timeArr?.length) {
@@ -75,192 +84,100 @@ const CreateAndUpdateModal = (props: any) => {
    }
 
 
-   function onSetName(): void {
-      const name = nameControl.current?.value;
-      if (typeof name === 'string') setName(name);
-   }
-
-
-   function onSetLocation(): void {
-      const loc = locationControl.current?.value;
-      if (typeof loc === 'string') setLocation(loc);
-   }
-
-
-   function onSetGuests(): void {
-      const gts = guestsControl.current?.value;
-      if (typeof gts === 'string') setGuests(gts);
-   }
-
-
-   function onSetDescription(): void {
-      const desc = descriptionControl.current?.value;
-      if (typeof desc === 'string') setDescription(desc);
-   }
-
-
-   function addToDatabase(ev: React.MouseEvent<HTMLElement>): void {
-      ev.preventDefault();
-      const errors: string[] = [];
-      const allTrue: boolean[] = [];
-      if (dateTime.year >= 2000 && (dateTime.month >= 0 && dateTime.month <= 11) && (dateTime.date >= 1 && dateTime.date <= 31)) {
-         allTrue.push(true);
-      } else {
-         errors.push('Date is required. Make sure its value is valid.');
-         allTrue.push(false);
-      }
-
-      if (dateTime.hour >= 0 && dateTime.hour <= 23 && dateTime.minute >= 0 && dateTime.minute <= 59) {
-         allTrue.push(true);
-      } else {
-         errors.push('Time is required. Make sure its value is valid.');
-         allTrue.push(false);
-      }
-
-      if (name) {
-         allTrue.push(true);
-      }
-      else {
-         errors.push('Name is required.');
-         allTrue.push(false);
-      }
-
-      if (!allTrue.includes(false)) {
-         const newEvent: UnsavedCalendarEvent = {
-            dateTime: new Date(dateTime.year, dateTime.month, dateTime.date, dateTime.hour, dateTime.minute),
-            name,
+   function isDateValid(date: DateTime) {
+      if (date.year) {
+         if (date.month >= 0 && date.month <= 11) {
+            if (date.day >= 1 && date.day <= 31) return true;
          }
-         if (location) newEvent.location = location;
-         if (guests) newEvent.guests = guests;
-         if (description) newEvent.description = description;
-         dispatch(createPostEventAction(newEvent));
-         props.closeWhenComplete();
-      } else {
-         setErrorList(errors);
       }
+
+      return false;
    }
 
-   return props.isOpen ? ReactDOM.createPortal(
+   function isTimeValid(time: DateTime) {
+      const { hour, minute } = time;
+      if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59) return true;
+
+      return false;
+   }
+
+   function saveToDatabase(data: UnsavedCalendarEvent | UnsavedTask | UnsavedReminder | UnsavedNote) {
+      if (type === EVENT) dispatch(createPostEventAction(data as UnsavedCalendarEvent));
+      if (type === TASK) dispatch(createPostTaskAction(data as UnsavedTask));
+      if (type === REMINDER) dispatch(createPostReminderAction(data as UnsavedReminder));
+   }
+
+   return isOpen ? ReactDOM.createPortal(
       <div
          id='modal-container'
          className='d-flex justify-content-center align-items-center'
-         onClick={ev => props.closePortal(ev)}
+         onClick={ev => closePortal(ev)}
       >
          <div className='add-new-portal'>
             <div className='form'>
                <div
                   className='form-close d-flex justify-content-center align-items-center'
-                  onClick={ev => props.closePortal(ev, true)}
+                  onClick={ev => closePortal(ev, true)}
                >
                   <VscClose />
                </div>
 
                <div className='form-header'>
-                  <h3>New {props.type}</h3>
+                  <h3>New {type}</h3>
                </div>
 
                {
-                  <div className='error-list'>
-                     {
-                        errorList.map((error, idx) => {
-                           if (idx + 1 === errorList.length) return <div key={idx} className='el-error'><div><BiError/></div> {error}</div>
-                           return <div key={idx} className='el-error' style={{marginBottom: 10}}><div><BiError/></div> {error}</div>
-                        })
-                     }
-                  </div>
+                  type === EVENT &&
+                  <EventForm
+                     type={type}
+                     dateInputValue={dateInputValue}
+                     onSetDate={onSetDate}
+                     timeInputValue={timeInputValue}
+                     onSetTime={onSetTime}
+                     isDateValid={isDateValid}
+                     isTimeValid={isTimeValid}
+                     saveToDatabase={saveToDatabase}
+                     closeWhenComplete={closeWhenComplete}
+                  />
                }
 
-               <Form>
-                  <Form.Row>
-                     <Col>
-                        <Form.Group controlId='date'>
-                           <Form.Label>Date</Form.Label>
-                           <Form.Control 
-                              as='input' 
-                              type='date' 
-                              ref={dateControl} 
-                              onChange={onSetDate}
-                              value={dateInputValue()}
-                           />
-                        </Form.Group>
-                     </Col>
-                     <Col>
-                        <Form.Group controlId='time'>
-                           <Form.Label>Time</Form.Label>
-                           <Form.Control 
-                              type='time'
-                              ref={timeControl}
-                              onChange={onSetTime}
-                              value={timeInputValue()}   
-                           ></Form.Control>
-                        </Form.Group>
-                     </Col>
-                  </Form.Row>
+               {
+                  type === TASK &&
+                  <TaskForm
+                     type={type}
+                     dateInputValue={dateInputValue}
+                     onSetDate={onSetDate}
+                     timeInputValue={timeInputValue}
+                     onSetTime={onSetTime}
+                     isDateValid={isDateValid}
+                     isTimeValid={isTimeValid}
+                     saveToDatabase={saveToDatabase}
+                     closeWhenComplete={closeWhenComplete}
+                  />
+               }
 
-                  <Form.Row>
-                     <Col>
-                        <Form.Group controlId='name'>
-                           <Form.Label>Name</Form.Label>
-                           <Form.Control
-                              as='input'
-                              type='text'
-                              ref={nameControl}
-                              onChange={onSetName}
-                              value={name}
-                           />
-                        </Form.Group>
-                     </Col>
-                  </Form.Row>
+               {
+                  type === REMINDER &&
+                  <ReminderForm
+                     type={type}
+                     dateInputValue={dateInputValue}
+                     onSetDate={onSetDate}
+                     timeInputValue={timeInputValue}
+                     onSetTime={onSetTime}
+                     isDateValid={isDateValid}
+                     isTimeValid={isTimeValid}
+                     saveToDatabase={saveToDatabase}
+                     closeWhenComplete={closeWhenComplete}
+                  />
+               }
 
-                  <Form.Row>
-                     <Col>
-                        <Form.Group controlId='location'>
-                           <Form.Label>Location</Form.Label>
-                           <Form.Control
-                              as='input'
-                              type='text'
-                              ref={locationControl}
-                              onChange={onSetLocation}
-                              value={location}
-                           />
-                        </Form.Group>
-                     </Col>
-                  </Form.Row>
+               {
+                  type === NOTE &&
+                  <NoteForm
+                     type={type}
+                  />
+               }
 
-                  <Form.Row>
-                     <Col>
-                        <Form.Group controlId='guests'>
-                           <Form.Label>Guests</Form.Label>
-                           <Form.Control
-                              as='input'
-                              type='text'
-                              ref={guestsControl}
-                              onChange={onSetGuests}
-                              value={guests}
-                           />
-                        </Form.Group>
-                     </Col>
-                  </Form.Row>
-
-                  <Form.Row>
-                     <Col>
-                        <Form.Group controlId='description'>
-                           <Form.Label>Description</Form.Label>
-                           <Form.Control
-                              as='textarea'
-                              type='text'
-                              ref={descriptionControl}
-                              onChange={onSetDescription}
-                              value={description}
-                           />
-                        </Form.Group>
-                     </Col>
-                  </Form.Row>
-
-                  <Button as='button' type='submit' onClick={ev => addToDatabase(ev)}>
-                     Create {props.type}
-                  </Button>
-               </Form>
             </div>
          </div>
       </div>,
